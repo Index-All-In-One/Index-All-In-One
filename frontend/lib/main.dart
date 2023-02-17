610c53ef29d44f69fcd95b969f18578e0a39ed82
RubyOcelot(loliw) <ruby_ocelot@outlook.com>
2023-02-16 22:59:19 -0800

    add fields keys

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -56,7 +56,7 @@ class _GFGState extends State<GFG> {
 
 class CustomSearchDelegate extends SearchDelegate {
   // Demo list to show querying
-  List<String> searchTerms = [
+  List<String> docNameList = [
     "A very loooooooooooooooooong file name",
     "Apple",
     "Banana",
@@ -67,6 +67,28 @@ class CustomSearchDelegate extends SearchDelegate {
     "Pineapples",
     "Strawberries"
   ];
+
+  List<String> documentFieldKeys = [
+    "source",
+    "doc_name",
+    "link",
+    "doc_type",
+    "file_size",
+    "created_date",
+    "modified_date",
+    "summary",
+  ];
+
+  Map<String, String> documentFieldDisplayNames = {
+    "doc_name": "Name/Title",
+    "doc_type": "Type",
+    "link": "Link",
+    "source": "Source",
+    "created_date": "Created Date",
+    "modified_date": "Modified Date",
+    "summary": "Summary",
+    "file_size": "Size",
+  };
 
   // first overwrite to
   // clear the search text
@@ -96,63 +118,75 @@ class CustomSearchDelegate extends SearchDelegate {
   // third overwrite to show query result
   @override
   Widget buildResults(BuildContext context) {
-    List<String> matchQuery = [];
+    List<Map<String, String>> queryResults = [];
     //TODO : add the logic to send request to backend and get result
-    //TODO : need new search result format.
-    for (var fruit in searchTerms) {
-      if (fruit.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(fruit);
+    for (var name in docNameList) {
+      if (name.toLowerCase().contains(query.toLowerCase())) {
+        queryResults.add({
+          "doc_name": name,
+          "doc_type": "<Document Type>",
+          "link": "<Link>",
+          "source": "<Source>",
+          "created_date": "<Created Date>",
+          "modified_date": "<Modified Date>",
+          "summary":
+              "With six children in tow, Catherine raced to the airport departing gate. This wasn't an easy task as the children had other priorities than to get to the gate. She knew that she was tight on time and the frustration came out as she yelled at the kids to keep up. They continued to test her, pretending not to listen and to move in directions that only slowed them down. They had no idea the wrath they were about to receive when Catherine made it to the gate only to be informed that they had all missed the plane.",
+          "file_size": "<File Size>"
+        });
       }
     }
-    // return ListView.builder(
-    //   itemCount: matchQuery.length,
-    //   itemBuilder: (context, index) {
-    //     var result = matchQuery[index];
-    //     return ListTile(
-    //       title: Text(result),
-    //     );
-    //   },
-    // );
     return ListView.builder(
-      itemCount: matchQuery.length + 1,
+      itemCount: queryResults.length + 1,
       itemBuilder: (BuildContext context, int index) {
         if (index == 0) {
           // return the header
           return ListTile(
-            title: Row(children: const <Widget>[
-              Expanded(child: Text("File Name")),
-              Expanded(child: Text("Source")),
-              Expanded(child: Text("Modified Time")),
-              Expanded(child: Text("Document Type")),
-              Expanded(child: Text("Summary")),
-            ]),
+            title: Row(
+                children: documentFieldKeys
+                    .map((key) => Expanded(
+                          child: Text(documentFieldDisplayNames[key]!),
+                        ))
+                    .toList()),
           );
         }
 
         // return row
-        var row = matchQuery[index - 1];
+        var singleQueryResult = queryResults[index - 1];
         return ListTile(
-          title: Row(children: <Widget>[
-            Expanded(
-                child: Tooltip(
-              child: Text(
-                row,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
-              message: row,
-            )),
-            Expanded(child: Text("<Source>")),
-            Expanded(child: Text("<Modified Time>")),
-            Expanded(child: Text("<Document Type>")),
-            Expanded(
-                child: Text(
-              "With six children in tow, Catherine raced to the airport departing gate. This wasn't an easy task as the children had other priorities than to get to the gate. She knew that she was tight on time and the frustration came out as she yelled at the kids to keep up. They continued to test her, pretending not to listen and to move in directions that only slowed them down. They had no idea the wrath they were about to receive when Catherine made it to the gate only to be informed that they had all missed the plane.",
-              softWrap: false,
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-            )),
-          ]),
+          title: Row(
+              children: documentFieldKeys.map((key) {
+            Expanded returnWidget;
+            switch (key) {
+              case 'doc_name':
+                returnWidget = Expanded(
+                  child: Tooltip(
+                    message: singleQueryResult[key]!,
+                    child: Text(
+                      singleQueryResult[key]!,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                );
+                break;
+              case 'summary':
+                returnWidget = Expanded(
+                  child: Text(
+                    singleQueryResult[key]!,
+                    softWrap: false,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                );
+                break;
+              default:
+                returnWidget = Expanded(
+                  child: Text(singleQueryResult[key]!),
+                );
+                break;
+            }
+            return returnWidget;
+          }).toList()),
         );
       },
     );
