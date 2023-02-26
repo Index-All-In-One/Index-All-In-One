4e837027016199c086b66e5cfa564c82adb3660e
RubyOcelot(loliw) <ruby_ocelot@outlook.com>
2023-02-26 01:08:06 -0800

    fix plugin_instance_id unique in request Table

--- backend-flask/plugin_instance_model_worker.py
+++ backend-flask/plugin_instance_model_worker.py
@@ -8,8 +8,15 @@ class Request(model):
     id = sqlalchemy_db.Column(sqlalchemy_db.Integer, primary_key=True)
     request_op = sqlalchemy_db.Column(sqlalchemy_db.String(20), nullable=False)
     plugin_name = sqlalchemy_db.Column(sqlalchemy_db.String(120), nullable=True)
-    plugin_instance_id = sqlalchemy_db.Column(sqlalchemy_db.String(50), unique=True, nullable=True)
+    plugin_instance_id = sqlalchemy_db.Column(sqlalchemy_db.String(50), nullable=True)
     update_interval = sqlalchemy_db.Column(sqlalchemy_db.Integer, nullable=True)
 
+class RunningPluginInstance(model):
+    __tablename__ = 'running_plugin_instance'
+    id = sqlalchemy_db.Column(sqlalchemy_db.Integer, primary_key=True)
+    request_op = sqlalchemy_db.Column(sqlalchemy_db.String(20), nullable=False)
+    plugin_name = sqlalchemy_db.Column(sqlalchemy_db.String(120), nullable=True)
+    plugin_instance_id = sqlalchemy_db.Column(sqlalchemy_db.String(50), unique=True, nullable=True)
+    update_interval = sqlalchemy_db.Column(sqlalchemy_db.Integer, nullable=True)
 
 __all__ = ['model', 'Request']
