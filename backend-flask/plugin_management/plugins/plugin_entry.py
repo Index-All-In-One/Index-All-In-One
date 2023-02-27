a1b58998e374fb0a642af4a15a8fd72e5dff0a4a
RubyOcelot(loliw) <ruby_ocelot@outlook.com>
2023-02-26 20:13:05 -0800

    add return for dispatch_plugin

--- backend-flask/plugin_management/plugins/plugin_entry.py
+++ backend-flask/plugin_management/plugins/plugin_entry.py
@@ -3,14 +3,14 @@ import importlib
 def dispatch_plugin_update(plugin_name, plugin_args):
     plugin_module = importlib.import_module("plugins.plugin_" + plugin_name)
     plugin_function = getattr(plugin_module, "plugin_" + plugin_name+ "_update")
-    plugin_function(*plugin_args)
+    return plugin_function(*plugin_args)
 
 def dispatch_plugin_init(plugin_name, plugin_args):
     plugin_module = importlib.import_module("plugins.plugin_" + plugin_name)
     plugin_function = getattr(plugin_module, "plugin_" + plugin_name+ "_init")
-    plugin_function(*plugin_args)
+    return plugin_function(*plugin_args)
 
 def dispatch_plugin_del(plugin_name, plugin_args):
     plugin_module = importlib.import_module("plugins.plugin_" + plugin_name)
     plugin_function = getattr(plugin_module, "plugin_" + plugin_name+ "_del")
-    plugin_function(*plugin_args)
+    return plugin_function(*plugin_args)
