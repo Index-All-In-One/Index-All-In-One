7cfaa2b452b3f2fa86a1447d85ee692be8a14b72
RubyOcelot(loliw) <ruby_ocelot@outlook.com>
2023-02-28 11:50:11 -0800

    add stub plugin and allow list

---
+++ backend-flask/plugin_management/plugins/plugin_stub.py
@@ -0,0 +1,8 @@
+def plugin_stub_update(plugin_instance_id):
+    print("Plugin stub update: ", plugin_instance_id)
+
+def plugin_stub_init(plugin_instance_id, plugin_init_info):
+    print("Plugin stub init: ", plugin_instance_id, plugin_init_info)
+
+def plugin_stub_del(plugin_instance_id):
+    print("Plugin stub del: ", plugin_instance_id)
