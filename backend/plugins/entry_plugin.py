09c9e399f45eb0b11ee75ee855aa31c75e62f947
RubyOcelot(loliw) <ruby_ocelot@outlook.com>
2023-03-02 02:57:48 -0800

    remove prefix in plugin dispatch

--- backend/plugins/entry_plugin.py
+++ backend/plugins/entry_plugin.py
@@ -8,13 +8,13 @@ allowed_plugins = [ \
 def get_plugin_list():
     return allowed_plugins
 
-def dispatch_plugin(path_prefix, function_name, plugin_name, plugin_args):
+def dispatch_plugin(function_name, plugin_name, plugin_args):
     #function_name: update, init, del
     if plugin_name not in allowed_plugins:
         # TODO: log error
         print("dispatch_plugin: Plugin not allowed: ", plugin_name)
         # TODO: return status
         return
-    plugin_module = importlib.import_module(path_prefix + "plugins.plugin_" + plugin_name)
+    plugin_module = importlib.import_module("plugins.plugin_" + plugin_name)
     plugin_function = getattr(plugin_module, "plugin_" + plugin_name+ "_" + function_name)
     return plugin_function(*plugin_args)
