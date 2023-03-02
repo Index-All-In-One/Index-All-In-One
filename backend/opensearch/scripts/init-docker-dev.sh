39234140b275e374036ecb5839ffea37ad1190df
RubyOcelot(loliw) <ruby_ocelot@outlook.com>
2023-03-02 03:15:41 -0800

    add sleep time arg for opensearch init

--- backend/opensearch/scripts/init-docker-dev.sh
+++ backend/opensearch/scripts/init-docker-dev.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-sleep 16
+# pass in the sleep time as an argument
+if [ -z "$1" ]
+then
+  arg1="15"
+else
+  arg1="$1"
+fi
+
+sleep $arg1
 python3 ./opensearch/create_index.py ./opensearch/index.json
