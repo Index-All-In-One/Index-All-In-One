38f304dd1134bb19b1309d63b77e2cf2c4c09124
RubyOcelot(loliw) <ruby_ocelot@outlook.com>
2023-03-04 12:57:03 -0800

    catch exception in create_index

--- backend/opensearch/create_index.py
+++ backend/opensearch/create_index.py
@@ -11,5 +11,10 @@ if __name__ == "__main__":
     if len(sys.argv) >= 2:
         index_file = sys.argv[1]
     logging.info("Loading index file %s into OpenSearch", index_file)
-    init_opensearch_db(index_file,host)
-    logging.info("Index file %s is loaded into OpenSearch", index_file)
+    try:
+        init_opensearch_db(index_file,host)
+    except Exception as e:
+        logging.error("Failed to load index file %s into OpenSearch", index_file)
+        logging.error(e)
+    else:
+        logging.info("Index file %s is loaded into OpenSearch", index_file)
