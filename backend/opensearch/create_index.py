df93ffb9d384f070110ac26608fd263fef5630b1
RubyOcelot(loliw) <ruby_ocelot@outlook.com>
2023-03-06 17:51:55 -0800

    add sleep and try for index init

--- backend/opensearch/create_index.py
+++ backend/opensearch/create_index.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import logging
+import time
 from conn import *
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -8,13 +9,20 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 if __name__ == "__main__":
     index_file = "index.json"
     host = os.environ.get('OPENSEARCH_HOSTNAME', 'localhost')
+    sleep_interval = 5
     if len(sys.argv) >= 2:
         index_file = sys.argv[1]
-    logging.info("Loading index file %s into OpenSearch", index_file)
-    try:
-        init_opensearch_db(index_file,host)
-    except Exception as e:
-        logging.error("Failed to load index file %s into OpenSearch", index_file)
-        logging.error(e)
-    else:
-        logging.info("Index file %s is loaded into OpenSearch", index_file)
+    if len(sys.argv) >= 3:
+        sleep_interval = (int)(sys.argv[2])
+
+    logging.info("Keep Trying Loading index file %s into OpenSearch", index_file)
+    while True:
+        try:
+            init_opensearch_db(index_file,host)
+        except Exception as e:
+            logging.error("Failed to load index file %s into OpenSearch", index_file)
+            logging.error(e)
+        else:
+            logging.info("Index file %s is loaded into OpenSearch", index_file)
+            break
+        time.sleep(sleep_interval)
--- backend/opensearch/scripts/init-docker-dev.sh
+++ backend/opensearch/scripts/init-docker-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# pass in the sleep time as an argument
+# pass in the initial sleep time as an argument
 if [ -z "$1" ]
 then
   arg1="15"
@@ -8,5 +8,13 @@ else
   arg1="$1"
 fi
 
+# pass in the sleep interval as an argument
+if [ -z "$2" ]
+then
+  arg2="5"
+else
+  arg2="$2"
+fi
+
 sleep $arg1
-python3 ./opensearch/create_index.py ./opensearch/index.json
+python3 ./opensearch/create_index.py ./opensearch/index.json $arg2
