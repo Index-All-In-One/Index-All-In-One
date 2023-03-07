cf76dfb8db5451afb1de5e67080ef73ff9b3f87b
RubyOcelot(loliw) <ruby_ocelot@outlook.com>
2023-03-06 18:49:16 -0800

    add comment in plugin_stub

--- backend/plugins/plugin_stub.py
+++ backend/plugins/plugin_stub.py
@@ -1,14 +1,21 @@
 from plugins.status_code import PluginReturnStatus
 
 def plugin_stub_update(plugin_instance_id,opensearch_hostname='localhost'):
+    # only do update job for this plugin, don't need to loop or sleep
+    # should accept opensearch_hostname as argument
+    # Ensure that if plugin_gmail_del has been executed, unfinished update won't be write to opensearch anymore
+
     print("Plugin stub update: ", plugin_instance_id, opensearch_hostname)
     return PluginReturnStatus.SUCCESS
 
 def plugin_stub_init(plugin_instance_id, plugin_init_info):
+    # do init jobs, such as store credentials in database
+    # plugin_init_info is a dict, defined in plugin_stub_info_list
     print("Plugin stub init: ", plugin_instance_id, plugin_init_info)
     return PluginReturnStatus.SUCCESS
 
 def plugin_stub_del(plugin_instance_id):
+    # do delete jobs, such as delete credentials in database
     print("Plugin stub del: ", plugin_instance_id)
     return PluginReturnStatus.SUCCESS
 
