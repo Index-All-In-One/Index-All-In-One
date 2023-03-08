98ed523979254d4b6da7ea2290dfe0b3ade07748
RubyOcelot(loliw) <ruby_ocelot@outlook.com>
2023-03-07 19:25:33 -0800

    docker compose file include web

--- frontend/lib/globals.dart
+++ frontend/lib/globals.dart
@@ -1,5 +1,7 @@
 library my_project.globals;
 
+import 'dart:io';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-final String baseUrl = dotenv.env['BASE_URL'] ?? 'http://127.0.0.1:5000';
+final String baseUrl = Platform.environment['BASE_URL'] ??
+    (dotenv.env['BASE_URL'] ?? 'http://127.0.0.1:5000');
--- frontend/scripts/run-web-in-docker.sh
+++ frontend/scripts/run-web-in-docker.sh
@@ -3,5 +3,5 @@
 # run this script from the root of frontend
 ./scripts/build-web-release.sh
 
-docker build -f docker-nginx/Dockerfile -t flutter-web .
-docker run -d --rm --name flutter-web-debug -p 8000:80 flutter-web
+docker build -f docker-nginx/Dockerfile -t iaio-flutter-web .
+docker run -d --rm --name iaio-flutter-web-debug -p 8000:80 flutter-web
