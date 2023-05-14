63172a943ff17d885a14aa0c6a7c7d79914d8539
RubyOcelot(loliw) <ruby_ocelot@outlook.com>
2023-05-13 20:16:27 -0700

    update flask debug script

--- backend/flask/scripts/flask-debug.sh
+++ backend/flask/scripts/flask-debug.sh
@@ -3,6 +3,10 @@
 # Set environment variables
 export FLASK_APP=flask/app.py
 export FLASK_DEBUG=1
+export FLASK_RUN_CERT=flask/ssl_cert/localhost.pem
+export FLASK_RUN_KEY=flask/ssl_cert/localhost-key.pem
+export GOAUTH_CLIENT_ID
+export GOAUTH_CLIENT_SECRET
 
 # Start the Flask application
 flask run --host=127.0.0.1 --port=5235
